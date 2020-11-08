ea6dabd80be54e7e441987822eb95a7a0d3810f8
Jonas Schweiger <jokassel@gmx.de>
2020-11-08 19:45:55 +0100

    [mod] remove Item from app
    It was a sample introduced by the template

--- backend/app/app/models/user.py
+++ backend/app/app/models/user.py
@@ -1,12 +1,7 @@
-from typing import TYPE_CHECKING
-
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
-
-if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
 
 
 class User(Base):
@@ -16,4 +11,3 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-    items = relationship("Item", back_populates="owner")
