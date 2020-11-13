d56ae4d3e04e80488f2a6e92239bb1b2c0634c0d
Jonas Schweiger <jokassel@gmx.de>
2020-11-13 08:36:35 +0100

    [add] model for a document

---
+++ backend/app/app/models/document.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+# from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+class Document(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    filename = Column(String, nullable=False)
