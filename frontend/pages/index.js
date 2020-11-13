e93b5b64b9442cfbd0d15ab4edb0534346a89283
Luke Dowling <lpdowling26@googlemail.com>
2020-11-13 19:56:13 +0100

    added basic styles and layout

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,38 @@
+import Head from "next/head";
+import mainStyles from "../styles/main.module.css";
 
-export default function Index() {
-
+export default function Home() {
   return (
     <div>
-      <p>
-        Hello SoundQuest!
-      </p>
+      <Head>
+        <title>SoundQuest</title>
+      </Head>
+      <main>
+        {/* logos */}
+
+        <div className={mainStyles.logoContainer}>
+          <div className={mainStyles.logoImgContainer}></div>
+          <div className={mainStyles.logoImgContainer}></div>
+        </div>
+
+        {/* search section */}
+
+        <div className={mainStyles.searchContainer}>
+          <div className={mainStyles.searchMain}>
+            <select className={mainStyles.searchSelect}></select>
+            <input
+              className={mainStyles.searchInput}
+              type="text"
+              placeholder="search"
+            />
+            <button className={mainStyles.searchBtn}>Go!</button>
+            {/* components go here */}
+          </div>
+          <div className={mainStyles.searchSide}>
+            {/* components go here */}
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
