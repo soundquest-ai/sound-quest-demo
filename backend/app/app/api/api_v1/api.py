38de18f2879a95f173236e4128e8f74c9ae129fb
Jonas Schweiger <jokassel@gmx.de>
2020-11-14 16:45:18 +0100

    [mod] disable unused apis

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -4,7 +4,7 @@ from app.api.api_v1.endpoints import login, users, utils
 from app.api.api_v1.endpoints.docs import doc
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
+# api_router.include_router(login.router, tags=["login"])
 api_router.include_router(doc.router, prefix="/docs", tags=["docs"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+# api_router.include_router(users.router, prefix="/users", tags=["users"])
+# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
