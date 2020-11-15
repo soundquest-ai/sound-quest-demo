f7ff3487cef6ac2e8c84a01db86c7437c817d9dd
Luke Dowling <lpdowling26@googlemail.com>
2020-11-15 16:45:36 +0100

    [mod] search function working

--- frontend/components/Form/FormComponent.js
+++ frontend/components/Form/FormComponent.js
@@ -1,60 +1,82 @@
 import React from "react";
 import styles from "./form.module.css";
-import DisplayData from "./ShowData";
+import stylesTwo from "./data.module.css";
 import useSWR from "swr";
 
 export default class FormComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      search: "",
-    };
+  constructor(props) {
+    super(props);
+    this.state = { value: "", search: "" };
+
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  search() {
-    const fetcher = (url) => fetch(url).then((res) => res.json());
-    const { data, error } = useSWR("/api/v1/docs", fetcher);
-    if (error) return <div>failed to load</div>;
-    if (!data) return <div>loading...</div>;
-    return <DisplayData item={data} />;
+  handleChange(event) {
+    this.setState({ value: event.target.value });
   }
 
-  // handleSubmit(){
-  //   this.setState({
-
-  //   })
-  // }
-
-  handleChange() {
-    this.setState({
-      [name]: value,
-    });
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ search: this.state.value });
   }
 
   render() {
-    // const requests = this.state.search.map((item) => (
-    //   <DisplayData key={item.id} item={item} handleChange={this.handleChange} />
-    // ));
-
     return (
-      <div className={styles.searchContainer}>
-        <div className={styles.searchMain}>
-          <select className={styles.searchSelect}>
-            <option>----</option>
-            <option>Page Numbers go here</option>
-          </select>
-          <input
-            className={styles.searchInput}
-            type="text"
-            name="search"
-            placeholder="search"
-          />
-          <button className={styles.searchBtn}>Go!</button>
-          {/* <div>{requests}</div> */}
+      <form onSubmit={this.handleSubmit}>
+        <div className={styles.searchContainer}>
+          <div className={styles.searchMain}>
+            <select className={styles.searchSelect}>
+              <option>----</option>
+              <option>Page Numbers go here</option>
+            </select>
+            <input
+              className={styles.searchInput}
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder="search"
+            />
+            <button onClick={this.handleClick} className={styles.searchBtn}>
+              Go!
+            </button>
+            <Data value={this.state.search} />
+          </div>
+          <div className={styles.searchSide}></div>
         </div>
-        <div className={styles.searchSide}></div>
-      </div>
+      </form>
     );
   }
+}
+
+function Data(props) {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(
+    `http://localhost:3000/api/v1/filter/?filter=${props.value}&skip=0&limit=100`,
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  const dataComponents = data.map((item) =>
+    props.value === "" ? (
+      <></>
+    ) : (
+      <DisplayData key={item.id} title={item.title} />
+    )
+  );
+  return <div>{dataComponents}</div>;
+}
+
+function DisplayData(props) {
+  return (
+    <div className={stylesTwo.container}>
+      <div>
+        <h3>Response: {props.title}!</h3>
+        <h3>Time: {Date()}!</h3>
+      </div>
+      {/* your music player goes here! */}
+      <div>Player </div>
+    </div>
+  );
 }
