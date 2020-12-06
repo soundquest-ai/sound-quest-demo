6e80e9a670609b65e2677b0b62f903634b8287a1
Jonas Schweiger <jokassel@gmx.de>
2020-12-06 15:58:13 +0100

    [mod] use react-player instead of react-soundplayer

--- frontend/components/MusicPlayer/Player.js
+++ frontend/components/MusicPlayer/Player.js
@@ -1,37 +1,6 @@
 import useSWR from "swr";
 import styles from "./player.module.css";
-import {
-  PlayButton,
-  VolumeControl,
-  Progress,
-  Timer,
-} from "react-soundplayer/components";
-import { withCustomAudio } from "react-soundplayer/addons";
-
-const AWSSoundPlayer = withCustomAudio((props) => {
-  const { trackTitle, currentTime, duration } = props;
-  // console.log(props.trackTime);
-
-  return (
-    <div className={styles.container}>
-      {/* <h2 className={styles.title}>{trackTitle}</h2> */}
-      <PlayButton className={styles.playBtn} {...props} />
-      <VolumeControl
-        className={styles.volumeControl}
-        buttonClassName={styles.btnVolumeControl}
-        rangeClassName={styles.rangeVolumeControl}
-        {...props}
-      />
-      <Progress
-        className={styles.progress}
-        innerClassName={styles.inner}
-        value={(currentTime / duration) * 100 || 0}
-        {...props}
-      />
-      <Timer className={styles.timer} {...props} />
-    </div>
-  );
-});
+import ReactPlayer from 'react-player';
 
 export default function Player(props) {
   // console.log(props.currentTime);
@@ -45,12 +14,8 @@ export default function Player(props) {
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
-  return (
-    <AWSSoundPlayer
-      streamUrl={streamUrl}
-      trackTitle={data.title}
-      // trackTime={currentTime}
-      preloadType="auto"
+    return (
+            <ReactPlayer url={streamUrl} controls={true}
     />
   );
 }
