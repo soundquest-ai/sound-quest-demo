3da8d6739605e786ec8b1777d0b58703b7edc15f
Jonas Schweiger <jokassel@gmx.de>
2020-12-20 13:58:58 +0100

    [mod] enable auto_coersion

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -1,3 +1,10 @@
+from sqlalchemy_utils import force_auto_coercion
+
+# setup auto coercion. This does type checking and value conversion at
+# assignment time and not only when entering the database.
+# https://sqlalchemy-utils.readthedocs.io/en/latest/listeners.html#sqlalchemy_utils.listeners.force_auto_coercion
+force_auto_coercion()
+
 from .user import User
 from .document import Document
 from .word import Word
