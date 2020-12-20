3854392afd8e0e3fecc49274c80887eaa2fc9b93
Jonas Schweiger <jokassel@gmx.de>
2020-12-20 18:18:40 +0100

    [mod] setup tsvector column

--- backend/app/app/models/document.py
+++ backend/app/app/models/document.py
@@ -1,7 +1,8 @@
 """ORM model for a document in the system"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Index
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils import TSVectorType
 
 # from sqlalchemy.orm import relationship
 
@@ -14,12 +15,24 @@ from .word import Word
 class Document(Base):
     """ORM model for a document"""
 
+    __table_args__ = (
+        Index(
+            "Document_fulltext_search_vector_idx",
+            "fulltext_search_vector",
+            postgresql_using="gin",
+        ),
+    )
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     filename = Column(String, nullable=True)
 
     fk_aws_transcription = Column(Integer, ForeignKey("aws_transcription.id"))
     transcription = relationship(AWSTranscription)
+
+    fulltext = Column(String, nullable=True)
+    fulltext_search_vector = Column(TSVectorType("fulltext"))
+    fulltext_regconfig = Column(String, nullable=True)
 
     words = relationship(Word, cascade="all, delete-orphan", order_by=Word.order)
 
