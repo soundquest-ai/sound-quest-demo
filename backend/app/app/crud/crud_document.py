4128acfe17149d21834bbd5e2be9630591aa513e
Jonas Schweiger <jokassel@gmx.de>
2021-01-03 16:52:55 +0100

    [mod] use websearch for the full text search

--- backend/app/app/crud/crud_document.py
+++ backend/app/app/crud/crud_document.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict, Optional, List
 
+from sqlalchemy import func
 from sqlalchemy.orm import Session
+
 
 from app.core.security import get_password_hash, verify_password
 from app.crud.base import CRUDBase
@@ -12,13 +14,21 @@ class CRUDDocument(CRUDBase[Document, DocumentCreate, DocumentUpdate]):
     def filter_document(
         self, db: Session, *, filter_str: str, skip: int = 0, limit: int = 100
     ) -> List[Document]:
-        return (
+
+        ts_query = func.websearch_to_tsquery(Document.fulltext_regconfig, filter_str)
+
+        # func.ts_headline(Document.fulltext_regconfig,
+        # ts_query,
+        # Document.fulltext)
+        res = (
             db.query(Document)
-            .filter(Document.fulltext_search_vector.match(filter_str))
+            .filter(Document.fulltext_search_vector.op("@@")(ts_query))
             .offset(skip)
             .limit(limit)
             .all()
         )
+
+        return res
 
 
 document = CRUDDocument(Document)
